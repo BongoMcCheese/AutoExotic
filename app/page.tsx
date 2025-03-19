1434800dbd38c110d7f6159b4ad422ff4f5118ae
BongoMcCheese <63338789+BongoMcCheese@users.noreply.github.com>
2025-03-18 23:37:58 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -51,6 +51,7 @@ export default async function Home() {
         spreadsheetId={spreadsheetId}
         sheetName={sheetName}
         errorMessage={errorMessage}
+        isDiabled={isDisabled}
       />
     </div>
   )
